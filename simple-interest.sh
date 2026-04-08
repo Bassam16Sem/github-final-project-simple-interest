b55eb70bfbb96a2a620a49e4cbce9779f4edf6f8
Bassam16Sem <bassamnuris@gmail.com>
2026-04-08 16:05:35 +0700

    Translate simple interest script prompts to English

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-# Skrip ini menghitung bunga sederhana (Simple Interest)
 
-echo "Masukkan pokok pinjaman (principal):"
+echo "Enter principal amount:"
 read p
-echo "Masukkan suku bunga per tahun (rate):"
+
+echo "Enter rate of interest:"
 read r
-echo "Masukkan periode waktu dalam tahun (time):"
+
+echo "Enter time (years):"
 read t
 
-s=$(expr $p \* $t \* $r / 100)
+si=$((p * r * t / 100))
 
-echo "Bunga sederhana adalah: "
-echo $s
+echo "Simple Interest is: $si"
